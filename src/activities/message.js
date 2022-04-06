b1c5769726d883693f510ce84adf545de8910126
larkdizy0 <103124956+larkdizy0@users.noreply.github.com>
2022-04-06 17:17:12 +0300

    Add files via upload

---
+++ src/activities/message.js
@@ -0,0 +1,20 @@
+const client = global.client
+const config = require('../configs/config')
+const ms = require('ms')
+
+module.exports = async(message) => {
+
+    if(message.author.bot || !message.guild) return;
+    const prefix = config.Prefixs.filter(x => message.content.startsWith(x))[0];
+    if(!prefix) return;
+    let command = message.content.split(" ")[0].slice(prefix.length);
+    let args = message.content.split(" ").slice(1);
+	//const commandName = args[0].toLowerCase();
+    const cmd = client.commands.get(command) || client.commands.array().find((x) => x.help.aliases && x.help.aliases.includes(command));
+    if(!cmd || (cmd.help.private === true && message.author.id !== config.OwnerID) || cmd.help.off) return
+    if(client.blockedFromCommand.includes(message.author.id)) return
+    cmd.execute(client, message, args)
+
+}
+
+module.exports.configurator = { name: "message" }
